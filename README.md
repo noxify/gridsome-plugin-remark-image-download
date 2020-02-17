6a54e6d09084c82184bac1b78c804f657e420a0e
Marcus Reinhardt <webstone@gmail.com>
2020-02-18 00:00:56 +0100

    initial load

---
+++ README.md
@@ -0,0 +1,58 @@
+# Gridsome Remark Plugin - Image Downloader
+
+Simple plugin for `@gridsome/transformer-remark` to enable the download of remote images.
+
+## Install
+
+```
+npm install -s https://github.com/noxify/gridome-plugin-remark-image-download
+
+```
+
+## Setup
+
+```js
+module.exports = {
+  siteName: 'Gridsome',
+  plugins: [
+    //...
+  ],
+  templates: {
+    //...
+  },
+  transformers: {
+    //Add markdown support to all file-system sources
+    remark: {
+      plugins: [
+        ['@noxify/gridsome-plugin-remark-image-download', {
+          targetPath: './src/assets/contentImages'
+        }]
+      ]
+    }
+  }
+}
+
+```
+
+## Configuration options
+
+`targetPath` Defines the target directory for the downloaded images If you set `./src/assets/contentImages`, it will save the images to `<projectroot>/src/assets/contentImages/`
+
+## Example Markdown
+
+```md
+---
+title: Markdown test file
+---
+
+## Images
+
+### Local Images
+
+![Image](./images/alexandr-podvalny-220262-unsplash.jpg)
+
+### Remote Images
+
+![Image](https://raw.githubusercontent.com/gridsome/gridsome-starter-blog/master/content/posts/images/alexandr-podvalny-220262-unsplash.jpg)
+
+```
---
+++ src/plugin.js
@@ -0,0 +1,87 @@
+const fs = require('fs')
+const path = require('path')
+const crypto = require('crypto')
+const imageType = require('image-type')
+const visit = require('unist-util-visit')
+const imageDownload = require('image-download')
+
+const urlRegExp = /^(?:([^:\/?#]+):)?(?:\/\/([^\/?#]*))?([^?#]*\/)?([^?#]*)?$/gi;
+
+module.exports = function attacher(options = {}) {
+
+    const transformer = this.data('transformer')    
+
+    return async function transform(tree, file) {
+        
+        const images = []
+
+        visit(tree, 'image', node => images.push(node))
+        
+        for (const node of images) {
+            let filePath;
+            const data = node.data || {}
+            const props = data.hProperties || {}
+            const classNames = props.class || []
+
+            
+                
+
+            let imageHTML = null
+            let noscriptHTML = null
+
+            try {
+
+                const isExternal = (urlRegExp.exec(node.url) !== null);
+
+                if (isExternal) {
+                    filePath = await getRemoteImage(node.url, options);
+                    filePath = transformer.resolveNodeFilePath(file.data.node, filePath)
+                } else {
+                    throw "Invalid Image URL";
+                }
+                
+                const asset = await transformer.assets.add(filePath, {
+                    alt: props.alt || node.alt,
+                    width: props.width,
+                    height: props.height,
+                    classNames,
+                    ...options
+                })
+
+                imageHTML = asset.imageHTML
+                noscriptHTML = asset.noscriptHTML
+            } catch (err) {
+                console.log(err);
+                return node.url
+            }
+
+            if (imageHTML) {
+                node.type = 'html'
+                node.value = imageHTML + noscriptHTML
+            }
+        }
+    }
+}
+
+async function getRemoteImage(url, options) {
+
+    const imagePath = await imageDownload(url).then(buffer => {
+
+        const hash = crypto.createHash('sha256');
+        hash.update(url);
+        var targetFileName = hash.digest('hex');
+
+        const type = imageType(buffer);
+
+        const filePath = path.resolve(
+            options.targetPath,
+            `${targetFileName}.${type.ext}`
+        )
+
+        fs.writeFile(filePath, buffer, (err) => console.log(err ? err : ''));
+        return filePath;
+    });
+        
+
+    return imagePath;
+}
